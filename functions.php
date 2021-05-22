96a62000409c755e2d9985dfc6d7678e4c034729
Somgur1 <Vincedewit@live.nl>
2021-05-22 10:51:48 +0200

    een begin aan project

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function dBConnect() {
+
+}
+
+
+function getHeader(){
+    $header = "<img class='img1' src='Foto/logo.png' alt='Lekker lezen logo'>";
+    $header .=  "<button class='navigation' onclick='document.location='index.html'>Homepage</button>";
+
+}
+
+
+?>
---
+++ layoutIndex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Lekker Lezen</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-widht, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+
+<body>
+<div class="header">
+<header><?= getHeader(); ?></header>
+</div>
+
+</body>
