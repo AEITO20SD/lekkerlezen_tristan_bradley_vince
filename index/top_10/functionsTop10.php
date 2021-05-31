674af38042be26222718094b8383014df31aa788
Somgur1 <Vincedewit@live.nl>
2021-05-31 09:29:53 +0200

    gedaan

--- index/top_10/functionsTop10.php
+++ index/top_10/functionsTop10.php
@@ -19,31 +19,42 @@ function dBConnect()
 }
 
 function showBoeken(){
+    $conn = new PDO("mysql: host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id, Naam, auteur, kaft FROM boek");
+  $stmt->execute();
+  
+  // set the resulting array to associative
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
     $x = 0;
-
-  foreach ($result as $boek) { ?>
-    <div class="container">
-      <div class="boekenboxinbox">
-        <?php $x++;
-        echo "$x: <br>";
-
-        ?>
-        <p><?php echo  $boek["Naam"] ?></p>
-        <?php echo '<img class="img3"src="data:image/jpeg;base64,' . base64_encode($boek['kaft']) . '"/>'; ?>
-
+  
+    foreach ($result as $boek) { ?>
+      <div class="container">
+        <div class="boekenboxinbox">
+          <?php $x++;
+          echo "$x: <br>";
+  
+          ?>
+          <p><?php echo  $boek["Naam"] ?></p>
+          <?php echo '<img class="img3"src="data:image/jpeg;base64,' . base64_encode($boek['kaft']) . '"/>'; ?>
+  
+        </div>
+        <div class="boekenbox">
+          <p><?php echo $boek["descriptie"] ?></p>
+  
+        </div>
       </div>
-      <div class="boekenbox">
-        <p><?php echo $boek["descriptie"] ?></p>
-
-      </div>
-    </div>
- }
+      <?php
+      return $boek;
+     }
 }
 
 function headerKnoppen()
 {
     $headerknop = "<img class='img1' src='../Foto/logo.png' alt='Lekker lezen logo'>";
-    $headerknop .=  "<button class='navigation' onclick='document.location='index.html'>Homepage</button>";
+    $headerknop .=  "<button class='navigation' onclick='document.location='../index/index.html'>Homepage</button>";
     $headerknop .=  "<button class='navigation' onclick='document.location='genre.php''>Genre</button>";
     $headerknop .= "<button class='navigation' onclick='document.location='top10.php''>Top 10</button>";
     $headerknop .=  "<button class='navigation' onclick='document.location='overons.html''>Over Ons</button>";
