412b86defa8742b60233db33e47fc4cbd2ffcb6f
Somgur1 <Vincedewit@live.nl>
2021-05-31 11:58:04 +0200

    boeken toegevoegd in html

--- top_10/functionsTop10.php
+++ top_10/functionsTop10.php
@@ -55,6 +55,8 @@ function showBoeken(){
         </div>
       </div>
       <?php
+
+      
       return $boek;
      }
     }
--- top_10/layoutTop10.php
+++ top_10/layoutTop10.php
@@ -18,12 +18,53 @@ $conn = dBConnect(); ?>
 
         </div>
 </header>
-<div class="box">
-<p>hoi</p>
-</div>
+
 
 <main>
-<?php showBoeken() ?>
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "lekkerlezen";
+  
+  $conn = new PDO("mysql: host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id, Naam, auteur, descriptie, kaft FROM boek");
+  $stmt->execute();
+  
+  // set the resulting array to associative
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  ?>
+</header>
+
+<body>
+  
+</head>
+
+<body>
+  <?php
+
+
+  $x = 0;
+
+  foreach ($result as $boek) { ?>
+    <div class="container">
+      <div class="boekenboxinbox">
+        <?php $x++;
+        echo "$x: <br>";
+        
+        ?>
+        <p><?php echo  $boek["Naam"] ?></p>
+        <?php echo '<img class="img3"src="data:image/jpeg;base64,' . base64_encode($boek['kaft']) . '"/>'; ?>
+
+      </div>
+      <div class="boekenbox">
+        <p><?php echo $boek["descriptie"] ?></p>
+
+      </div>
+    </div>
+  <?php }
+  ?>
 </main>
 
 
