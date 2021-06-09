2fd50e9b26c6d9a8cd1810502a5ec662976cc45e
Tristan2002 <71229711+Tristan2002@users.noreply.github.com>
2021-06-09 15:44:32 +0200

    divided genre by functions

--- Genre/LayoutGenre.php
+++ Genre/LayoutGenre.php
@@ -1,29 +1,67 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-
 <head>
-  <title> Fietsenwinkel </title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/fietsenwinkel.css">
+  <meta name="description" content="Lekker Lezen site">
+  <title>Lekker Lezen</title>
+  <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-  <?php
-  dBconnect()
-  ?>
-  <div class="header">
-  <header><?=getHeader();?></header>
-</div>
-  <nav><?=getNav();?></nav>
-  <div class="main">
-    <aside class="asideLeft"><?=getAside('left');?></aside>
-    <section><?=getSection();?></section>
-    <aside class="asideRight"><?=getAside('right');?></aside>
+  <header>
+    <div class="header">
+      <img class="img1" src="Foto/logo.png" alt="Lekker lezen logo">
+      <button class="navigation" onclick="document.location='index/index.php'">Homepage</button>
+      <button class="navigation" onclick="document.location='genre.php'">Genre</button>
+      <button class="navigation" onclick="document.location='top10.php'">Top 10</button>
+      <button class="navigation" onclick="document.location='overons.html'">Over Ons</button>
+      <button class="navigation" onclick="document.location='contact.php'">Contact Pagina</button>
+    </div>
+  </header>
+  <main>
+  <div class="center">
+    <h2> Genre </h2>
   </div>
-  <div class="footer">
-  <footer><?=getFooter();?></footer>
-</div>
-</body>
+  <div class="container">
+    <div class="genre"> </div>
+    <div class="genre"> </div>
+  </div>
+  <div class="container">
+    <div class="genre"> <a href="genre.php?genre=Biografie"> Biografie </a>
+    </div>
+    <div class="genre"> <a href="genre.php?genre=Humor"> Humor </a>
+    </div>
+  </div>
+  <div class="container">
+    <div class="genre"> <a href="genre.php?genre=Literair"> Literaire essay </a>
+    </div>
+    <div class="genre"> <a href="genre.php?genre=Psychologisch"> Psychologisch verhaal </a>
+    </div>
+  </div>
+  <div class="container">
+    <div class="genre"> <a href="genre.php?genre=Fictie"> Fictie </a>
+    </div>
+    <div class="genre"> <a href="genre.php?genre=Historisch"> Historisch verhaal </a>
+    </div>
+  </div>
+  <div class="container">
+    <div class="genre"> <a href="genre.php?genre=Science Fiction"> Science Fiction </a>
+    </div>
+    <div class="genre"> <a href="genre.php?genre=Thriller"> Thriller </a>
+    </div>
+  </div>
+  <?php
+  dBConnect();
 
-</html>
+  $books = getBookByGenre();
+  showBookByGenre($books, $genre);
+  ?>
+</main>
+  <footer>
+    <div class="contactbox">
+      <h3>Contact:</h3>
+      <p>Tel: 06060606<br>
+        E-mail: Lekkerlezen@lezen.nl<br>
+        Adres: Lezenstraat 7
+      </p>
+    </div>
+  </footer>
+</body>
--- Genre/functionsgenre.php
+++ Genre/functionsgenre.php
@@ -9,45 +9,52 @@ function dBconnect(){
 
   $conn = new PDO("mysql: host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("SELECT id, Naam, auteur, kaft FROM boek");
-  $stmt->execute();
 
-  // set the resulting array to associative
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $conn;
 }
 
- catch ( PDOexception $e ) {
-   echo $e->getmessage();
-   echo "kon geen verbinding maken.";
+function getBookByGenre(){
+  try {
+    if (isset($_GET["genre"])) {
+      $conn = dBConnect();
+      $genre = $_GET["genre"];
+      $stmt = $conn->prepare("SELECT bookid, Naam, auteur, descriptie, genre, kaft FROM boek WHERE genre = :genre");
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      $stmt->execute(array('genre' => $genre));
+      $result = $stmt->fetchAll();
+
+      return $result;
+    }
+    } catch (PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  
+    $conn = null;
 }
 
-function getHeader(){
-return "dit is de header";
-}
-function getFooter(){
-  return "dit is de footer";
-  }
-function getNav (){
-      <img class="img1" src="Foto/logo.png" alt="Lekker lezen logo">
-      <button class="navigation" onclick="document.location='index.html'">Homepage</button>
-      <button class="navigation" onclick="document.location='genre.html'">Genre</button>
-      <button class="navigation" onclick="document.location='top10.php'">Top 10</button>
-      <button class="navigation" onclick="document.location='overons.html'">Over Ons</button>
-      <button class="navigation" onclick="document.location='contact.php'">Contact Pagina</button>
+function showBookByGenre($genre, $books) {
+  ?>
+      <div class="center">
+        <h2><?php echo $genre ?></h2>
+      </div>
+      <?php
+      foreach ($books as $boek) { ?>
+        <div class="container">
+          <div class="boekenboxinbox">
+            <p><?php echo $boek["Naam"] ?></p>
+            <?php echo '<img class="img3"src="data:image/jpeg;base64,'.base64_encode( $boek['kaft'] ).'"/>'; ?>
 
-return $menu;
+          </div>
+          <div class="boekenbox">
+            <p><?php $id = $boek["bookid"];
+            echo $boek["descriptie"] ?></p>
+            <p><?php echo "<a href='reviews.php?bookid=$id'>Link to reviews</a>"; ?></p>
+          </div>
+        </div>
+        <?php
+      }
 }
-function getAside(){}
 
-function getPage(){
 
- }
- return $page;
-}
-function getSection(){
-
- }
- return $section;
-}
 
 ?>
