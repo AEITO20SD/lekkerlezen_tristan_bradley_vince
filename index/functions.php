7ff5fa67b5e73531f05db4a567670ebfd34d9b9b
Somgur1 <Vincedewit@live.nl>
2021-06-11 13:27:53 +0200

    knoppen veranderd

--- index/functions.php
+++ index/functions.php
@@ -18,12 +18,12 @@ function dBConnect() {
 
 function headerKnoppen(){
     $headerknop = "<img class='img1' src='../Foto/logo.png' alt='Lekker lezen logo'>";
-    $headerknop .=  "<button class='navigation' onclick='document.location='index.html'>Homepage</button>";
-    $headerknop .=  "<button class='navigation' onclick='document.location='genre.php''>Genre</button>";
-    $headerknop .= "<button class='navigation' onclick='document.location='top10.php''>Top 10</button>";
-    $headerknop .=  "<button class='navigation' onclick='document.location='overons.html''>Over Ons</button>";
-    $headerknop .= "<button class='navigation' onclick='document.location='contact.php''>Contact Pagina</button>";
-    $headerknop .= "<button class='navigation' onclick='document.location='inlog.html''>Inlog pagina</button>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/index/'>Homepage</a>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/Genre/LayoutGenre.php'>Genre</a>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/top_10/layoutTop10.php'>Top 10</a>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/over_ons/'>Over Ons</a>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/contact/'>Contact pagina</a>";
+    $headerknop .= "<a class='navigation' href='http://localhost/lekkerlezen_tristan_bradley_vince/inlog/'>Inlog pagina</a>";
 
     return $headerknop;
 }
